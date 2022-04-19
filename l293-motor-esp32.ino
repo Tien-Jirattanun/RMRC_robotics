c61ec6c34272b7219d3c7e49f57e9a4a615e8b81
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2022-04-19 11:07:55 +0700

    Create l293-motor-esp32.ino

---
+++ l293-motor-esp32.ino
@@ -0,0 +1,58 @@
+#define MOTOR1_PIN1 27
+#define MOTOR1_PIN2 13
+#define MOTOR2_PIN1 19
+#define MOTOR2_PIN2 18
+#define MOTOR3_PIN1 14
+#define MOTOR3_PIN2 12
+#define MOTOR4_PIN1 4
+#define MOTOR4_PIN2 15
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(MOTOR1_PIN1, OUTPUT);
+  pinMode(MOTOR1_PIN2, OUTPUT);
+  pinMode(MOTOR2_PIN1, OUTPUT);
+  pinMode(MOTOR2_PIN2, OUTPUT);
+}
+
+void loop() {
+
+  moveForward();
+  delay(2000);
+  moveLeft();
+  delay(2000);
+  moveRight();
+  delay(2000);
+  motorStop();
+  delay(2000);
+  
+
+}
+
+void moveForward(){
+  digitalWrite(MOTOR1_PIN1, HIGH);
+  digitalWrite(MOTOR1_PIN2, LOW);
+  digitalWrite(MOTOR2_PIN1, HIGH);
+  digitalWrite(MOTOR2_PIN2, LOW);
+}
+
+void moveLeft(){
+  digitalWrite(MOTOR1_PIN1, LOW);
+  digitalWrite(MOTOR1_PIN2, HIGH);
+  digitalWrite(MOTOR2_PIN1, HIGH);
+  digitalWrite(MOTOR2_PIN2, LOW);
+}
+
+void moveRight(){
+  digitalWrite(MOTOR1_PIN1, HIGH);
+  digitalWrite(MOTOR1_PIN2, LOW);
+  digitalWrite(MOTOR2_PIN1, LOW);
+  digitalWrite(MOTOR2_PIN2, HIGH);
+}
+
+void motorStop(){
+  digitalWrite(MOTOR1_PIN1, LOW);
+  digitalWrite(MOTOR1_PIN2, LOW);
+  digitalWrite(MOTOR2_PIN1, LOW);
+  digitalWrite(MOTOR2_PIN2, LOW);
+}
