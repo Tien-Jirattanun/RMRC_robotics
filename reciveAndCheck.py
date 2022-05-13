3023b434c2241c88684500c51e2c343a2caa7a68
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2022-05-13 11:32:13 +0700

    Update reciveAndCheck.py

--- reciveAndCheck.py
+++ reciveAndCheck.py
@@ -1,37 +1,33 @@
+import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
 
 message = ""
 
 def on_message(client, userdata, message):
     print(str(message.payload.decode("utf-8")))
     message = str(message.payload.decode("utf-8"))
+    if 'JoyAxisMotion' in message:
+        axis = int(message[63])
+        valueJoy = float(message[75:79])
 
-mqttBroker ="mqtt.eclipseprojects.io"
+    elif 'JoyButtonDown' in message:
+        button = int(message[64])
 
-client = mqtt.Client("Raspi")
-client.connect(mqttBroker) 
-
-client.loop_start()
-
-client.subscribe("JOYSTICK")
-client.on_message=on_message
-
-if 'JoyAxisMotion' in message:
-    axis = int(message[63])
-    valueJoy = float(message[75:79])
-
-elif 'JoyButtonDown' in message:
-    button = int(message[64])
-
-    if button == 0:
-        print("A")
-    elif button == 1:
-        print("B")
-    elif button == 2:
-        print("X")
-    elif button == 3:
-        print("Y")
+        if button == 0:
+            print("A")
+        elif button == 1:
+            print("B")
+        elif button == 2:
+            print("X")
+        elif button == 3:
+            print("Y")
         
     elif 'JoyHatMotion' in message:
         valueArrow = message[73:79]
@@ -61,6 +57,17 @@ elif 'JoyButtonDown' in message:
             GPIO.output(13, GPIO.LOW)
             GPIO.output(19, GPIO.LOW)
             GPIO.output(26, GPIO.LOW)
+
+mqttBroker ="mqtt.eclipseprojects.io"
+
+client = mqtt.Client("Raspi")
+client.connect(mqttBroker) 
+
+client.loop_start()
+
+client.subscribe("JOYSTICK")
+client.on_message=on_message
+
 
 time.sleep(300)
 client.loop_stop()
