6e2778fe308e035fb31ee2129ae4540fcf3b65e7
B0SSHUB <106664360+B0SSHUB@users.noreply.github.com>
2022-06-01 17:28:30 +0700

    fix defult value

--- main_ros/function.ino
+++ main_ros/function.ino
@@ -33,8 +33,8 @@ void moveLeft(int speed){
 
 void armBegin(){
   
-  armOne = 800;
-  armTwo = 155
+  armOne = 840;
+  armTwo = 200;
   
   ax12a.move(BASE_ARM, baseArm);
   ax12a.move(ARM_ONE, armOne);
